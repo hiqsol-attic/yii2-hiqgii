585f6deb7d2a63d0cbf7a7634f3dc01b2faf431d
Andrii Vasyliev <sol@hiqdev.com>
2015-04-19 14:18:22 +0000

    doc

--- Module.php
+++ Module.php
@@ -8,7 +8,19 @@
 namespace hiqdev\hiqgii;
 
 /**
- * HiQDev Gii
+ * HiQDev Specific Gii
+ *
+ * Basic usage: in config:
+ * ~~~
+ *     'modules' => [
+ *         'gii' => 'hiqdev\hiqgii\Module',
+ *     ],
+ * ~~~
+ *
+ * In console:
+ * ~~~
+ * ./yii gii/extension --packageName=yii2-thememanager --title='Theme Manager for Yii 2' --description='Component and widgets to manage themes' --namespace='hiqdev\thememanager\' --keywords='theme,manager,yii2,extension'
+ * ~~~
  */
 class Module extends \yii\gii\Module implements \yii\base\BootstrapInterface
 {
